47a6ec1afd00b325f9fbebdb0e3cee21d8247379
gui <guisarria@gmail.com>
2024-10-24 19:20:41 -0300

    feat: Suspense on searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+import { Suspense } from "react"
 import { PackageList } from "@/components/packages-list"
 import { QuerySection } from "@/components/query-section"
 import { SearchParamsProps } from "@/lib/types"
@@ -6,8 +6,20 @@ import { SearchParamsProps } from "@/lib/types"
 export default async function Home({ searchParams }: SearchParamsProps) {
   return (
     <div className="flex h-full w-full mb-4 lg:mb-14 justify-between gap-8 gap-y-12 p-12 2xl:px-60 flex-col lg:flex-row">
-      <QuerySection />
+      <Suspense fallback={<QuerySectionSkeleton />}>
+        <QuerySection />
+      </Suspense>
       <PackageList searchParams={searchParams} />
+    </div>
+  )
+}
+
+function QuerySectionSkeleton() {
+  return (
+    <div className="flex flex-col h-full items-center relative animate-pulse">
+      <div className="h-full w-full lg:w-[30vw] xl:w-[25vw] bg-muted rounded-t" />
+      <div className="w-full h-12 bg-muted rounded-b" />
+      <div className="w-1/3 h-12 bg-muted rounded mt-6 lg:absolute lg:-bottom-20" />
     </div>
   )
 }
