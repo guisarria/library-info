3dd89177654eb2cda87f6bf3fcf161749a11e862
Guilherme Sarria <guisarria@gmail.com>
2025-12-27 03:36:18 -0300

    refactor

--- src/components/ambient-light.tsx
+++ src/components/ambient-light.tsx
@@ -1,9 +1,7 @@
-"use client"
-
 const AmbientLight = () => {
   return (
     <div className="ambient-light -z-10">
-      <div className="light-a back" />
+      <div className="light-a" />
       <div className="light-b" />
       <div className="light-c" />
     </div>
